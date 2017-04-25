6138bebedd3caaa7ce4db5a3d36516300af5c813
Emil Boman <emil.boman@gmail.com>
2017-04-26 00:34:35 +0200

    Fixed name mangling of test source.

--- main.cpp
+++ main.cpp
@@ -21,14 +21,19 @@
 
 class Pinger {
 public:
-    Pinger(hyker::riks::RiksKit&& riks1, hyker::riks::RiksKit&& riks2) :
-            m_riks1 (std::move(riks1)),
-            m_riks2 (std::move(riks2)) {
+    hyker::riks::RiksKit m_riks1;
+    hyker::riks::RiksKit m_riks2;
+    int s = 0;
+    
+    Pinger(const std::string& riks1, const std::string& riks2) :
+            m_riks1 (initRiksKit(riks1)),
+            m_riks2 (initRiksKit(riks2)) {
         std::cout << m_riks1.getUID() << " - " << m_riks2.getUID() << std::endl;
     }
 
     void ping(const hyker::riks::Message& message) {
         std::cout << "Encrypting " << std::to_string(++s) << std::endl;
+        
         m_riks1.encryptMessage(message, "namespace", [&](const std::string& encrypted) {
             std::cout << "Decrypting " << std::to_string(++s) << std::endl;
             m_riks2.decryptMessage(encrypted,
@@ -41,41 +46,38 @@ public:
         });
     }
 
-    int s = 0;
-    hyker::riks::RiksKit m_riks1;
-    hyker::riks::RiksKit m_riks2;
-};
+    hyker::cryptobox::Cryptobox initCryptobox() {
+        return hyker::cryptobox::Cryptobox('#' + hyker::cryptobox::SecurityUtil::generateString(10), "asdqwe");
+    }
 
-hyker::cryptobox::Cryptobox initCryptobox() {
-    return hyker::cryptobox::Cryptobox('#' + hyker::cryptobox::SecurityUtil::generateString(10), "asdqwe");
-}
+    hyker::riks::RiksWhitelist initRiksWhitelist() {
+        return hyker::riks::RiksWhitelist([](const std::string& uid, const std::string& messageNamespace, const std::string& keyID) -> bool {
+            return true;
+        });
+    }
 
-hyker::riks::RiksWhitelist initRiksWhitelist() {
-    return hyker::riks::RiksWhitelist(
-            [](const std::string& uid, const std::string& messageNamespace, const std::string& keyID) -> bool {
-                return true;
+    hyker::riks::RiksKit initRiksKit(const std::string& file) {
+        auto rikskit = [&]() {
+            try {
+                return hyker::riks::RiksKit::load(file, "asdqwe", initRiksWhitelist());
+            } catch (...) {
+                return hyker::riks::RiksKit(initCryptobox(), initRiksWhitelist());
             }
-    );
-}
+        }();
 
-hyker::riks::RiksKit initRiksKit(const std::string& file) {
-    auto rikskit = [&](){
-        try {
-            return hyker::riks::RiksKit::load(file, "asdqwe", initRiksWhitelist());
-        } catch (...) {
-            return hyker::riks::RiksKit(initCryptobox(), initRiksWhitelist());
-        }
-    }();
-    sleep(1);
-    return rikskit;
-}
+        sleep(1);
+
+        return rikskit;
+    }
+
+};
 
 int main() {
 
     auto file1 = "riks1a.cache";
     auto file2 = "riks1b.cache";
 
-    Pinger pinger(initRiksKit(file1), initRiksKit(file2));
+    Pinger pinger(file1, file2);
 
     sleep(1);
 
