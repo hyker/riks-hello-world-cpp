360cd23744043bef87073985d12cb56c7be64ea1
Emil Boman <emil.boman@gmail.com>
2017-05-02 17:27:57 +0200

    Updated configurations and builds.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -Ldeps -Ideps/include main.cpp -lriks -lssl -lcrypto -o main.o -Wl,-rpath,deps -std=c++11 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -Ldeps -Ideps/include main.cpp -lriks -lssl -lcrypto -o main.o -Wl,-rpath,deps -std=c++11
