2737dfcfdabee60017b20c730a0be9189da953d1
Emil Boman <emil.boman@gmail.com>
2018-05-02 16:48:33 +0200

    Added delegate client example.

--- build-local.sh
+++ build-local.sh
@@ -4,7 +4,15 @@ rm -rf build
 mkdir build
 cd build
 
-g++ -L../../riks-cpp/build -I../../riks-cpp/src ../main.cpp -lriks -o main.o -Wl,-rpath,../../riks-cpp/build -pthread -std=c++11
+g++ \
+  -L../../riks-cpp/build/lib \
+  -I../../riks-cpp/src \
+  ../main.cpp \
+  -lriks \
+  -o main.o \
+  -Wl,-rpath,../../riks-cpp/build/lib \
+  -pthread \
+  -std=c++11
 
 cp ../res/default.config .
 cp ../res/root_certificate.pem .
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include "hyker/rikskit.hpp"
+#include "hyker/delegate_client.hpp"
+#include "hyker/exception.hpp"
 #include "hyker/log.hpp"
 
 #include <iostream>
@@ -7,138 +8,28 @@
 
 int main() {
     try {
-        using namespace hyker;
-        using namespace hyker::riks;
+        const hyker::DelegateClient delegate_client{
+            "key-delegate",
+            1234,
+            "72019E5042C24C90D23091ED74272360",
+            false,
+            "UNTRUSTED_API_KEY"
+        };
 
-        const auto uid = "#alice-00004";
-        const auto uid_2 = "#bob-00004";
+        const auto token = "199005157756";
 
-        //Log::setLogConditions(Log::LEVEL_VERBOSE);
-        
-        // Then, let's generate a UID for you.
-        
-        // Then, give your password.
-        const auto password = "asdqwe"; // No way.
-
-        // Then, define your whitelist.
-        Whitelist whitelist{[](std::string uid, std::string message_namespace, std::string key_id) -> Future<bool> {
-            Future<bool> access_granted;
-            std::thread{[access_granted]() mutable {
-                // std::cout << "Granting access in ";
-                // for (int i = 5; i > 0; --i) {
-                //     std::cout << std::to_string(i) << "...";
-                //     std::this_thread::sleep_for(std::chrono::seconds{1});
-                // }
-                // std::cout << "\n\n";
-                
-                // We are very naïve, we trust everyone.
-                access_granted = true;
-            }}.detach();
-
-            return access_granted;
-        }};
-
-        // Then, define your configurations file. (Optional)
-        const auto config = "default.config";
-        
-        // Finally, create a RIKS kit.
-        RiksKit rikskit_1(uid, password, whitelist, config);
-
-        // Now, we are ready to begin encrypting! Create a message.
-        const Message message("some secret data", "some immutable plaintext", "some mutable plaintext");
-
-        // Define the namespace of the message.
-        const auto message_namespace = "Earth 3";
-
-        // Encrypt your message:
-        const auto encrypted_message = rikskit_1.encrypt(message, message_namespace);
-
-        // DONE! You are ready to do whatever you wish with this data, nothing more needs to be done.
-        // But what about decrypting it? Let's start another RIKS kit.
-        // Generate another UID.
-        
-        // Give the password
-        const auto password_2 = "asdqwe";
-        
-        // Define another whitelist.
-        Whitelist whitelist_2{[](std::string uid, std::string message_namespace, std::string key_id) -> bool {
-            // Don't let any Martians read your messages.
-            if (message_namespace != "Earth 3") return false;
-            
-            // Only allow users with an ID starting with #hyker:
-            if (uid.find("#hyker") != 0) return false;
-            
-            // They are good. Give them access to the key.
-            return true;
-        }};
-
-        // Create your second RIKS kit. (Use custom config.)
-        RiksKit rikskit_2(uid_2, password_2, whitelist_2, {
-            {"storage_path",                 "../test_data"},
-            {"msg_host",                     "dev.msg.hykr.io"},
-            {"msg_port",                     1443},
-            {"msg_api_key",                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"},
-            {"kds_host",                     "dev.kds.hykr.io"},
-            {"kds_port",                     443},
-            {"kds_cache_expiration",         -1},
-            {"kds_api_key",                  "UNTRUSTED_API_KEY"},
-            {"kds_root_certificate",         "-----BEGIN CERTIFICATE-----\r\nMIIBoDCCAUWgAwIBAgIJALJOHjjAY42bMAoGCCqGSM49BAMCMCwxKjAoBgNVBAMM\r\nIUhZS0VSX0RFVkVMT1BNRU5UX0NBX0RPX05PVF9UUlVTVDAeFw0xNzEwMDQxNDQw\r\nNTJaFw0yNzEwMDIxNDQwNTJaMCwxKjAoBgNVBAMMIUhZS0VSX0RFVkVMT1BNRU5U\r\nX0NBX0RPX05PVF9UUlVTVDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABG0jjTQs\r\nFCnTYOpRik3qIOuZkGvet9Z+3mzVhhhMdJ4Oc+v5+8dR8qqSskyq4YytO+H7dySq\r\nLxU4pG4L2rzqmUCjUDBOMB0GA1UdDgQWBBSpUu5tEEvb5NxolVI5jauQljSb8jAf\r\nBgNVHSMEGDAWgBSpUu5tEEvb5NxolVI5jauQljSb8jAMBgNVHRMEBTADAQH/MAoG\r\nCCqGSM49BAMCA0kAMEYCIQCNpMt38MCafJcbo1OPzhI9AAavCpUHJbDHW9+YLbvz\r\nwQIhAI0ZRPhOsGsNMWm0pDq91c7yx4jUcpsHZTjR80iof1EQ\r\n-----END CERTIFICATE-----"},
-            {"replay_protector_window_size", 1000},
-            {"key_relay_enabled",            false}
-        });
-        
-        // NOW! Let's try decrypting the message.
-        const Message decrypted_message = rikskit_2.decrypt(encrypted_message);
-        for (int i = 0; true; ++i) {
-            struct SecretData {
-                int a;
-                double b;
-                bool c;
-            };
-
-            SecretData secret_data_struct;
-            secret_data_struct.a = 13;
-            secret_data_struct.b = 546.123;
-            secret_data_struct.c = true;
-		
-            //if (i % (1 * 128) == 0) {
-	    	  rikskit_1.rekey("namespace2");
-            //}
-
-            const auto encrypted_message = rikskit_1.encrypt({{&secret_data_struct, sizeof(SecretData)}, "immutable data", "mutable data"}, "namespace2");
-            rikskit_1.preshareKeyspace(uid_2, "namespace2");
-	    const auto decrypted_message = rikskit_2.decrypt(encrypted_message).get();
-
-            const auto secret_data           = *reinterpret_cast<const SecretData*>(decrypted_message.secret_data.begin());
-            const std::string immutable_data = decrypted_message.immutable_data;
-            const std::string mutable_data   = decrypted_message.mutable_data;
-
-            //std::cout << '\n';
-            //std::cout << "Secret data.a   " << secret_data.a  << '\n';
-            //std::cout << "Secret data.b:  " << secret_data.b  << '\n';
-            //std::cout << "Secret data.c:  " << secret_data.c  << '\n';
-            //std::cout << "Immutable data: " << immutable_data << '\n';
-            //std::cout << "Mutable data:   " << mutable_data   << '\n';
-            std::cout << encrypted_message.get().c_str() << '\n';
-
-            //std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        { // Preregister
+        const auto session = delegate_client.open();
+        const auto result = session.preregister(token);
+        std::cout << result.dump(4) << '\n';
         }
-        
-        // Now you know the basics. Here are some exercises for you:
-        // 
-        // - a) Change the namespace you encrypt in. What happens?
-        //   b) Change the allowed namespaces in the second rikskit.
-        // 
-        // - a) Change your user IDs. What happens?
-        //   b) Change the allowed user IDs in the second rikskit.
-        // 
-        // - Encrypt and return a message to the first rikskit, then decrypt it.
-        // 
-        // - Make the whitelist of the first rikskit more restrictive. 
-        // 
-        // - Trigger a rekey event. You can do this by doing the following call:
-        //       std::string message_namespace = "Earth";
-        //       rikskit2.rekey(message_namespace);
+
+        { // Collect
+        const auto session = delegate_client.open();
+        const auto result = session.collect(token);
+        std::cout << result.dump(4) << '\n';
+        }
+
     } catch (hyker::Exception& e) {
         std::cout << "Error: " << e.what() << '\n';
     }
